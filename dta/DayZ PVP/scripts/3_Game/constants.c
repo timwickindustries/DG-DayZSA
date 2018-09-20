c25afb321c988000a890782fa0fa000eb6b072f6
timwickindustries <timwickindustries@gmail.com>
2018-09-20 13:33:55 -0700

    Unlimited Stamina

--- dta/DayZ PVP/scripts/3_Game/constants.c
+++ dta/DayZ PVP/scripts/3_Game/constants.c
@@ -314,28 +314,28 @@ const int		ITEMSGEN_ITEMS_AMOUNT = 5;
  * @{
  */
 	// unit = currently percent (stamina max is 100)
-const int 	STAMINA_DRAIN_STANDING_SPRINT_PER_SEC = 5; //in units (how much sprint depletes stamina)
-const int 	STAMINA_DRAIN_CROUCHED_SPRINT_PER_SEC = 1; //in units (how much sprint in crouch depletes stamina)
-const int 	STAMINA_DRAIN_PRONE_SPRINT_PER_SEC = 3; //in units (how much sprint in prone depletes stamina)
-const float	STAMINA_DRAIN_HOLD_BREATH = 0.2; //in units (how much holding breath depletes stamina)
-const int	STAMINA_DRAIN_JUMP = 20;		// in units (how much jumping depletes stamina)
-const int	STAMINA_DRAIN_MELEE_LIGHT = 5; //in units (how much light melee punch depletes stamina)
-const float	STAMINA_DRAIN_MELEE_HEAVY = 20; //in units (how much heavy melee punch depletes stamina)
-const int	STAMINA_DRAIN_MELEE_EVADE = 8; // in units (how much evade depletes stamina)
+const int 	STAMINA_DRAIN_STANDING_SPRINT_PER_SEC = 0; //in units (how much sprint depletes stamina)
+const int 	STAMINA_DRAIN_CROUCHED_SPRINT_PER_SEC = 0; //in units (how much sprint in crouch depletes stamina)
+const int 	STAMINA_DRAIN_PRONE_SPRINT_PER_SEC = 0; //in units (how much sprint in prone depletes stamina)
+const float	STAMINA_DRAIN_HOLD_BREATH = 0; //in units (how much holding breath depletes stamina)
+const int	STAMINA_DRAIN_JUMP = 0;		// in units (how much jumping depletes stamina)
+const int	STAMINA_DRAIN_MELEE_LIGHT = 0; //in units (how much light melee punch depletes stamina)
+const float	STAMINA_DRAIN_MELEE_HEAVY = 0; //in units (how much heavy melee punch depletes stamina)
+const int	STAMINA_DRAIN_MELEE_EVADE = 0; // in units (how much evade depletes stamina)
 	
 const int 	STAMINA_GAIN_JOG_PER_SEC = 2; //in units (how much of stamina units is gained while jogging)
 const int 	STAMINA_GAIN_WALK_PER_SEC = 4; //in units (how much of stamina unitsis gained while walking)
 const int 	STAMINA_GAIN_IDLE_PER_SEC = 1; //in units (how much of stamina unitsis gained while iddling)
 const float STAMINA_GAIN_BONUS_CAP = 3.0; //in units (tells how much extra units can be added at best to stamina regain)
 
-const float STAMINA_KG_TO_STAMINAPERCENT_PENALTY = 2.5; //in units (by how many  units is max stamina bar reduced for each 1 kg of load weight)
+const float STAMINA_KG_TO_STAMINAPERCENT_PENALTY = 0; //in units (by how many  units is max stamina bar reduced for each 1 kg of load weight)
 const float STAMINA_MIN_CAP = 5; //in units (overload won't reduce max stamina bar under this value)
 const float STAMINA_SPRINT_THRESHOLD = 25; //in units (how many units of stamina you need regained in order to be able to start sprinting)
 const float STAMINA_HOLD_BREATH_THRESHOLD = 10; // in units
 const float STAMINA_JUMP_THRESHOLD = 20; // in units
 const float STAMINA_MELEE_HEAVY_THRESHOLD = STAMINA_DRAIN_MELEE_HEAVY; // in units (how many units we need to make a heavy hit in melee)
 const float STAMINA_MELEE_EVADE_THRESHOLD = 8; // in units
-const float STAMINA_REGEN_COOLDOWN_DEPLETION = 0.45; // in secs (how much time we will spend in cooldown before the stamina will starts with regeneration)
+const float STAMINA_REGEN_COOLDOWN_DEPLETION = 0.01; // in secs (how much time we will spend in cooldown before the stamina will starts with regeneration)
 const float STAMINA_REGEN_COOLDOWN_EXHAUSTION = 0.5;
 const float STAMINA_WEIGHT_LIMIT_THRESHOLD = 5000; //! in grams (weight where the player is not penalized by stamina)
 const float STAMINA_KG_TO_GRAMS = 1000; //for kg to g conversion
